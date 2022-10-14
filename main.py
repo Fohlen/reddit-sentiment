bc6f36938091d475a39a87411421eb04f4b33968
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2022-10-14 22:53:41 +0100

    More modularization and introduce sentiment

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from typing import Callable
 
 import requests
 import zstandard
+from textblob import TextBlob
 
 COMMENTS_URL = "https://files.pushshift.io/reddit/comments"
 ARCHIVE_TEMPLATE = "RC_{year}-{month}.zst"
@@ -23,18 +24,30 @@ def download_file(url: str, file_path: pathlib.Path):
                 fp.write(chunk)
 
 
-def decompress_archive(archive_path: pathlib.Path, callback: Callable[[str], None]):
-    with archive_path.open("rb") as fp:
+def decompress_archive(
+        archive_path: pathlib.Path,
+        output_path: pathlib.Path,
+        callback: Callable[[str, io.TextIOWrapper], None]
+):
+    with archive_path.open("rb") as fp, output_path.open("wt") as tp:
         dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
         with dctx.stream_reader(fp) as stream_reader:
             text_stream = io.TextIOWrapper(stream_reader, encoding='utf-8')
             for line in text_stream.readlines():
-                callback(line)
+                callback(line, tp)
 
 
-def process_line(line: str):
+def process_line(line: str, output: io.TextIOWrapper):
     document = json.loads(line)
-    print(document["body"], file=sys.stdout)
+    blob = TextBlob(document["body"])
+    print(
+        document["id"],
+        document["subreddit"],
+        document["created_utc"],
+        *blob.sentiment,
+        sep="\t",
+        file=output
+    )
 
 
 # Press the green button in the gutter to run the script.
@@ -43,9 +56,10 @@ if __name__ == '__main__':
     month = 12
     archive = ARCHIVE_TEMPLATE.format(year=year, month=month)
     version_path = pathlib.Path.cwd() / archive
+    output_path = pathlib.Path.cwd() / f"{archive}.tsv"
     version_url = f"{COMMENTS_URL}/{archive}"
 
     if not version_path.exists():
         download_file(version_url, version_path)
 
-    decompress_archive(version_path, process_line)
+    decompress_archive(version_path, output_path, process_line)
