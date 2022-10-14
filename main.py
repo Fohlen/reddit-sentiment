30f8d26855e466b3e325a628fc7504b417d0301f
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2022-10-14 22:40:54 +0100

    Modularize code a bit more

--- main.py
+++ main.py
@@ -5,35 +5,47 @@
 import io
 import json
 import pathlib
-import tempfile
+import sys
+from typing import Callable
 
 import requests
 import zstandard
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+COMMENTS_URL = "https://files.pushshift.io/reddit/comments"
+ARCHIVE_TEMPLATE = "RC_{year}-{month}.zst"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    versions = ["RC_2005-12"]
-    url = "https://files.pushshift.io/reddit/comments"
-    path = pathlib.Path.cwd() / f"{versions[0]}.zst"
-    version_url = f"{url}/{versions[0]}.zst"
-
-    with requests.get(version_url, stream=True) as response, path.open("wb") as fp:
+def download_file(url: str, file_path: pathlib.Path):
+    with requests.get(url, stream=True) as response, file_path.open("wb") as fp:
         response.raise_for_status()
         for chunk in response.iter_content():
             if chunk:
                 fp.write(chunk)
 
-    with path.open("rb") as fp:
+
+def decompress_archive(archive_path: pathlib.Path, callback: Callable[[str], None]):
+    with archive_path.open("rb") as fp:
         dctx = zstandard.ZstdDecompressor(max_window_size=2147483648)
         with dctx.stream_reader(fp) as stream_reader:
             text_stream = io.TextIOWrapper(stream_reader, encoding='utf-8')
             for line in text_stream.readlines():
-                print(line)
+                callback(line)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def process_line(line: str):
+    document = json.loads(line)
+    print(document["body"], file=sys.stdout)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    year = 2005
+    month = 12
+    archive = ARCHIVE_TEMPLATE.format(year=year, month=month)
+    version_path = pathlib.Path.cwd() / archive
+    version_url = f"{COMMENTS_URL}/{archive}"
+
+    if not version_path.exists():
+        download_file(version_url, version_path)
+
+    decompress_archive(version_path, process_line)
