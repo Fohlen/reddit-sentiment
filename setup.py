e88204fc8b3896302c420c06e1baa65fec8dbb05
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2022-10-22 21:31:33 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='reddit_sentiment',
+    version='1.0',
+    description='A reddit sentiment analysis toolkit',
+    author='Lennard Berger',
+    url='https://github.com/Fohlen/reddit-sentiment',
+    include_package_data=True,
+    packages=['reddit_sentiment'],
+ )
